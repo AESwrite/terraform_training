bdda8d45301204084ec2f97c9bb4f1fd9c67572d
AESwrite <aeswrite@gmail.com>
2019-02-18 15:31:08 +0300

    servers update 2

--- servers.tf
+++ servers.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "bastion" {
     ami = "ami-0664a710233d7c148"
     instance_type = "t2.micro"
+    subnet_id="${aws_subnet.public_subnet_for_bastion_host.id}"
 
     tags {
         Name = "bastion-host"
